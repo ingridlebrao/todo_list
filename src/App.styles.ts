a90bea6579beda645db4f2d6ab0b9efb582a1bbb
Ingrid Lebrão <ingridlebrao@gmail.com>
2022-07-14 18:10:22 -0300

    changing app and app styles

---
+++ src/App.styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #fff0f5;
+  color: #797a81;
+  min-height: 100vh;
+  font-family: 'IBM Plex Mono', monospace;
+`;
+
+export const Area = styled.div`
+  margin: auto;
+  max-width: 980px;
+  padding: 10px;
+`;
+
+export const Header = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-family: 'Baloo 2', monospace;
+  color: #000;
+  text-align: center;
+  border-bottom: 1px solid #444;
+  padding-bottom: 20px;
+`;
