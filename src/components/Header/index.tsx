3546e0a2a9e53d987714ef6e764aafb0355340bf
Ingrid Lebrão <ingridlebrao@gmail.com>
2022-07-14 18:10:49 -0300

    changing header component and style

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import * as C from './styles'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+
+    <C.Container>
+    <h4>inga.</h4>
+    <ul>
+      <li>home</li>
+      <li>contato</li>
+    </ul>
+    </C.Container>
+    </div>
+  )
+}
+
